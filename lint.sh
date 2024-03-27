9e8e67a3de96a5efccbc3ab3ef0106b3bcb13ae6
Maor Rozenfeld <49363375+maor-rozenfeld@users.noreply.github.com>
2024-03-27 08:30:15 +0100

    Sigh

--- lint.sh
+++ lint.sh
@@ -8,8 +8,10 @@
 # Match regex title with the title
 if grep -qP -- "$TITLE_REGEX" <<< "$TITLE"; then
   echo "Title matches regex: $TITLE_REGEX"
+  echo "verdict=Passed" >> "$GITHUB_OUTPUT"
 else
   echo "Title does not match regex: $TITLE_REGEX" >&2
   echo "::error title=PR Title is invalid::$ERROR_MESSAGE"
+  echo "verdict=$ERROR_MESSAGE" >> "$GITHUB_OUTPUT"
   exit 1
 fi
